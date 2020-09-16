14c08b4a583622d03a59b6b51938b71360ea1adf
super <supeng842499467@gmail.com>
2020-09-16 21:02:22 +0800

    init

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/jinzhu/gorm v1.9.16
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/spf13/viper v1.7.1
+	github.com/streadway/amqp v1.0.0
 	go.uber.org/zap v1.16.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
---
+++ pkg/rabbitmq/base.go
@@ -0,0 +1,68 @@
+package rabbitmq
+
+import (
+	"Traffic-data-processing/config"
+	"Traffic-data-processing/pkg/logging"
+	"fmt"
+	"github.com/streadway/amqp"
+	"go.uber.org/zap"
+)
+
+/**
+* @Author: super
+* @Date: 2020-09-16 21:00
+* @Description:
+**/
+
+var logger = logging.GetLogger()
+
+type RabbitMQ struct {
+	conn    *amqp.Connection
+	Channel *amqp.Channel
+
+	//队列名称
+	QueueName string
+	//交换机
+	Exchange string
+	//key
+	Key string
+	//连接信息
+	Mqurl string
+}
+
+// 创建RabbitMQ实例
+func NewRabbitMQ(queuqName string,
+	exchange string, key string) *RabbitMQ {
+
+	rabbitMQUrl, err := config.GetRabbitMQUrl()
+	if err != nil{
+		logger.Error("get rabbitmq url error", zap.Error(err))
+		panic(err)
+	}
+	rabbitmq := &RabbitMQ{
+		QueueName: queuqName,
+		Exchange:  exchange,
+		Key:       key,
+		Mqurl:     rabbitMQUrl,
+	}
+	//创建rabbitmq连接
+	rabbitmq.conn, err = amqp.Dial(rabbitmq.Mqurl)
+	rabbitmq.FailOnErr(err, "创建连接错误")
+	rabbitmq.Channel, err = rabbitmq.conn.Channel()
+	rabbitmq.FailOnErr(err, "获取channel失败")
+	return rabbitmq
+}
+
+// 断开channel和connection的连接释放资源
+func (r *RabbitMQ) Destory() {
+	r.Channel.Close()
+	r.conn.Close()
+}
+
+//自定义错误处理函数
+func (r *RabbitMQ) FailOnErr(err error, message string) {
+	if err != nil {
+		logger.Error(message, zap.Error(err))
+		panic(fmt.Sprintf("%s:%s", message, err))
+	}
+}
