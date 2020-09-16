854b2003fc12b8752c5aff805475a5b8ad40da27
super <supeng842499467@gmail.com>
2020-09-16 20:20:16 +0800

    init

---
+++ model/road.go
@@ -0,0 +1,13 @@
+package model
+
+/**
+* @Author: super
+* @Date: 2020-09-16 20:18
+* @Description:
+**/
+
+type Road struct {
+	TTiId   int    `json:"tti_id"`
+	TTIName string `json:"tti_name"`
+	WKTRoad string `json:"wkt_road"`
+}
