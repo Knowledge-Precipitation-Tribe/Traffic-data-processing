a89a14299344dc4fcb2ce232cffe764b2db8af99
super <supeng842499467@gmail.com>
2020-09-16 20:10:00 +0800

    init

--- model/shortRoad.go
+++ model/shortRoad.go
@@ -1,5 +1,7 @@
 package model
 
+import "fmt"
+
 /**
 * @Author: super
 * @Date: 2020-09-15 20:09
@@ -7,8 +9,22 @@ package model
 **/
 
 type ShortRoad struct {
-	TTiId   int
-	TTIName string
-	Start   string
-	Stop    string
+	TTiId          int    `json:"tti_id"`
+	TTIName        string `json:"tti_name"`
+	StartLongitude string `json:"start_longitude"`
+	StartLatitude  string `json:"start_latitude"`
+	StopLongitude  string `json:"stop_longitude"`
+	StopLatitude   string `json:"stop_latitude"`
+}
+
+func (road ShortRoad) String() string{
+	return fmt.Sprintf("TTIID: %d\n" +
+		"TTINAME: %s\n" +
+		"StartLongitude: %s\n" +
+		"StartLatitude: %s\n" +
+		"StopLongitude: %s\n" +
+		"StopLatitude: %s\n",
+		road.TTiId, road.TTIName,
+		road.StartLongitude, road.StartLatitude,
+		road.StopLongitude, road.StopLatitude)
 }
